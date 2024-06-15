cea6d0f9edf1019012bdc7afa461f598aff7ec3d
M4rilyn24 <166310787+M4rilyn24@users.noreply.github.com>
2024-06-14 22:57:59 -0500

    Update calc.py

--- app/calc.py
+++ app/calc.py
@@ -1,4 +1,5 @@
 import app
+import math
 
 
 class InvalidPermissions(Exception):
@@ -7,11 +8,13 @@ class InvalidPermissions(Exception):
 
 class Calculator:
     def add(self, x, y):
-        self.check_types(x, y)
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+            raise TypeError("Los argumentos deben ser de tipo int o float")
         return x + y
 
-    def substract(self, x, y):
-        self.check_types(x, y)
+
+    def subtract(self, x, y):
+        self.check_types(x, y)  # Verificar que x e y sean números
         return x - y
 
     def multiply(self, x, y):
@@ -22,19 +25,39 @@ class Calculator:
         return x * y
 
     def divide(self, x, y):
-        self.check_types(x, y)
+        self.check_types(x, y)  # Verificar que x e y sean números
         if y == 0:
-            raise TypeError("Division by zero is not possible")
-
+            raise ValueError("Division by zero is not possible")
         return x / y
 
     def power(self, x, y):
         self.check_types(x, y)
         return x ** y
 
-    def check_types(self, x, y):
-        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
-            raise TypeError("Parameters must be numbers")
+    def square_root(self, x):
+        self.check_type(x)
+        if x < 0:
+            raise ValueError("Square root is not defined for negative numbers")
+        return math.sqrt(x)
+
+    def log_base_10(self, x):
+        self.check_type(x)
+        if x <= 0:
+            raise ValueError("Logarithm is not defined for non-positive numbers")
+        return math.log10(x)
+
+    def check_types(self, *args):
+        for arg in args:
+            try:
+                float_arg = float(arg)  # Convertir el argumento a float
+            except ValueError:
+                raise TypeError("Parameters must be numbers")
+            if not isinstance(float_arg, (int, float)):
+                raise TypeError("Parameters must be numbers")
+
+    def check_type(self, x):
+        if not isinstance(x, (int, float)):
+            raise TypeError("Parameter must be a number")
 
 
 if __name__ == "__main__":  # pragma: no cover
