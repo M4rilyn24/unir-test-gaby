fb2d0b65b6cdcfa93684d2c13eafbcee02245230
M4rilyn24 <166310787+M4rilyn24@users.noreply.github.com>
2024-06-14 22:56:56 -0500

    Update api.py

--- app/api.py
+++ app/api.py
@@ -1,5 +1,4 @@
 import http.client
-
 from flask import Flask
 
 from app import util
@@ -24,10 +23,32 @@ def add(op_1, op_2):
         return (str(e), http.client.BAD_REQUEST, HEADERS)
 
 
-@api_application.route("/calc/substract/<op_1>/<op_2>", methods=["GET"])
-def substract(op_1, op_2):
+@api_application.route("/calc/subtract/<op_1>/<op_2>", methods=["GET"])
+def subtract(op_1, op_2):
     try:
         num_1, num_2 = util.convert_to_number(op_1), util.convert_to_number(op_2)
-        return ("{}".format(CALCULATOR.substract(num_1, num_2)), http.client.OK, HEADERS)
+        return ("{}".format(CALCULATOR.subtract(num_1, num_2)), http.client.OK, HEADERS)
     except TypeError as e:
         return (str(e), http.client.BAD_REQUEST, HEADERS)
+
+
+@api_application.route("/calc/square_root/<num>", methods=["GET"])
+def square_root(num):
+    try:
+        num = util.convert_to_number(num)
+        return ("{}".format(CALCULATOR.square_root(num)), http.client.OK, HEADERS)
+    except (TypeError, ValueError) as e:
+        return (str(e), http.client.BAD_REQUEST, HEADERS)
+
+
+@api_application.route("/calc/log_base_10/<num>", methods=["GET"])
+def log_base_10(num):
+    try:
+        num = util.convert_to_number(num)
+        return ("{}".format(CALCULATOR.log_base_10(num)), http.client.OK, HEADERS)
+    except (TypeError, ValueError) as e:
+        return (str(e), http.client.BAD_REQUEST, HEADERS)
+
+
+if __name__ == "__main__":
+    api_application.run(debug=True)
